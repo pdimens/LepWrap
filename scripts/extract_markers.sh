feb1f3bd557ca12792c9cf0ca6f24c9164405893
Pavel V. Dimens <pdimens@live.com>
2021-04-28 14:02:11 -0400

    Update extract_markers.sh

--- scripts/extract_markers.sh
+++ scripts/extract_markers.sh
@@ -1,7 +1,18 @@
 #! /usr/bin/env bash
 
+if [[ -z "$1" ]]; then
+cat <<EOF
+
+Extracts marker information (contig and position) from a Lep-Map3 data file.
+
+[usage]:	extract_markers.sh <data_file.gz>
+[example]: 	scripts/extract_markers.sh data_f.call.gz
+
+EOF
+  exit 1
+fi
+
 file=$(echo $1)
 echo "Extracting the marker names and positions from the LepMap3 data file ($file)"
 
 zcat $1 | awk '(NR>=7){print $1"\t"$2}' > snps.txt
-
