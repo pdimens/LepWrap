948da2d7087a85939ba0f7145c0f668abfa405e4
Pavel Dimens <pdimens@live.com>
2021-05-06 14:40:57 -0400

    improve script

--- scripts/refine_map.sh
+++ scripts/refine_map.sh
@@ -3,22 +3,39 @@
 # use this script to refine a particular map in an attempt to split out
 # markers from a particular linkage group. The loop iterators over LOD
 
+if [[ -z "$1" ]]; then
+cat <<EOF
+Attempt to refine a map from SeparateChromosomes2 by splitting out markers from an overclustered linkage group.
+[usage]:	refine_map.sh <mapfile> <linkage group> <LOD start> <LOD end> <size limit>
+[example]: 	scripts/refine_map.sh 3_SeparateChromosomes/map.31 1 22 70 30
+EOF
+  exit 1
+fi
+
 mkdir -p refine_map
 
+echo "# The maps in this folder were refined based on the map below" > refine_map/chosen.map
+echo $1 >> refine_map/chosen.map
+
 # the map you want to refine
-TARGETMAP=map.26
+#TARGETMAP=map.31
+TARGETMAP=$1
 # the linkage group you want to try to split
-TARGETLG=1
+#TARGETLG=1
+TARGETLG=$2
 # minimum LOD score for iterating
-LODMIN=22
+#LODMIN=22
+LODMIN=$3
 # maximum LOD score for iterating
-LODMAX=70
+#LODMAX=70
+LODMAX=$4
 # remove linkage groups with markers less than this amount
-SIZELIM=30
+#SIZELIM=30
+SIZELIM=$5
 
 for i in $(seq $LODMIN $LODMAX); do
-    zcat data_f.call.gz | java -cp LM3 SeparateChromosomes2 data=- map=maps.splitchrom/$TARGETMAP lg=$TARGETLG sizeLimit=$SIZELIM lodLimit=$i distortionLod=1 numThreads=10 > refine_map/map.$i
+    zcat 2_Filtering/data_f.call.gz | java -cp LM3 SeparateChromosomes2 data=- map=$TARGETMAP lg=$TARGETLG sizeLimit=$SIZELIM lodLimit=$i distortionLod=1 numThreads=10 > refine_map/map.$i
 done
 
 # generate a summary of the results
-scripts/map_summary_refine.sh refine_map
+scripts/map_summary.sh refine_map
