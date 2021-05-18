78805661e9832f579064d22ff18e8176c668c096
Pavel Dimens <pdimens@live.com>
2021-05-18 11:23:59 -0400

    update scripts

--- scripts/lg_cutoff.sh
+++ scripts/lg_cutoff.sh
@@ -6,9 +6,9 @@ cat <<EOF
 Unassign markers from linkage groups if their LG is greater than a specified value.
 Essentially, set an LG maximum, and markers above it will be assigned to group 0 (singles).
 
-[usage]:	purge_lg.sh <map.file> <LG max>
+[usage]:	lg_cutoff.sh <map.file> <LG max>
 # unassign all markers in LG's 25+
-[example]: 	scripts/purge_lg.sh 3_SeparateChromosomes/map.13 24
+[example]: 	scripts/lg_cutoff.sh 3_SeparateChromosomes/map.13 24
 
 EOF
   exit 1
@@ -16,11 +16,11 @@ fi
 
 echo "Converting markers from $1 in linkage groups >$2 to singles (0)"
 
-awk -v lim=$2 '{if (NR==1 || $1<=lim) print; else print 0}' $1 > $1.purged
+awk -v lim=$2 '{if (NR==1 || $1<=lim) print; else print 0}' $1 > $1.cutoff
 
-echo "Done! See the file $1.purged for the results."
+echo "Done! See the file $1.cutoff for the results."
 echo "Here is a summary of the outcome:"
 
-sort $1.purged | uniq -c | sort -k2n | tail -n +2
+sort $1.cutoff | uniq -c | sort -k2n | tail -n +2
 
 
